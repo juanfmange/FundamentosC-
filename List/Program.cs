4c597b2e35cf9719aefdb790148cf117217d02be
juanfmange <juanfmange@gmail.com>
2023-10-31 01:37:35 -0600

    Foreach at list

--- List/Program.cs
+++ List/Program.cs
@@ -9,6 +9,7 @@ namespace List
             //Variables globales
             string elem;
             int option;
+            int i;
             List<string> Personas = new List<string>();
             
             //Agregamos objetos en la lista
@@ -16,13 +17,16 @@ namespace List
             Personas.Add("Hugo");
             Personas.Add("Paco");
             Personas.Add("Erick");
+            
+            //Console.WriteLine("El numero de elementos en la lista es: {0}", Personas.Count());
+            
             do
             {
-                Console.WriteLine("1.- Agregar elemento");
-                Console.WriteLine("2: Salir");
-
+                Console.WriteLine("1. Agregar elemento");
+                Console.WriteLine("2. Salir");
+            
                 option = Convert.ToInt32(Console.ReadLine());
-
+            
                 if (option == 1)
                 {
                     Console.WriteLine("Ingresa un elemento a agregar a la lista");
@@ -30,12 +34,22 @@ namespace List
                     
                     Personas.Add(elem);
                 }
-
-            } while (option == 1);
-
-            {
                 
+            } while (option == 1);
+            // Mostramos los elementos de la lista
+            Console.WriteLine("Lista de elementos: \n");
+            // for (i = 0; i < Personas.Count(); i++)
+            // {
+            //     Console.WriteLine(Personas[i]);
+            // }
+            //foreach para recorrer una coleccion
+
+            foreach (string elemento in Personas)
+            {
+                Console.WriteLine(elemento);
             }
+            
+            
             
             
         }
